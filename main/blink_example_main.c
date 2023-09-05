26938ce0d8078727c19e433ee89422669195fae9
Zann181 <sazambranoc@unal.edu.co>
2023-09-05 12:32:14 -0500

    Actualizacion

--- main/blink_example_main.c
+++ main/blink_example_main.c
@@ -20,11 +20,6 @@ void tarea1(void* arg)
     int button_state;
     while (1)
     {
-       gpio_set_level(LED_GPIO, 1);
-        vTaskDelay(pdMS_TO_TICKS(500)); // Enciende el LED durante 500 ms
-        gpio_set_level(LED_GPIO, 0);
-        vTaskDelay(pdMS_TO_TICKS(500)); // Apaga el LED durante 500 ms
-
         button_state = gpio_get_level(BUTTON_GPIO);
 
         // Estado del botón ha cambiado, enviarlo a la cola
@@ -42,7 +37,7 @@ void tarea2(void* z)
 
     while (1)
     {
-        if (xQueueReceive(buffer, &rcv, pdMS_TO_TICKS(1000)) == pdTRUE) // Si recibe el valor dentro de 1 segundo, muestra en pantalla
+        if (xQueueReceive(buffer, &rcv, pdMS_TO_TICKS(100)) == pdTRUE) // Si recibe el valor dentro de 1 segundo, muestra en pantalla
         {
 
             printf("Item recibido: %lu\n", rcv); // Muestra el valor en pantalla
@@ -51,21 +46,22 @@ void tarea2(void* z)
                 while (1)
                 {
                     vTaskDelay(pdMS_TO_TICKS(500)); // Pequeña pausa para evitar rebotes del botón
-                     xQueueReceive(buffer, &rcv, pdMS_TO_TICKS(1000)); // Espera hasta que haya un elemento en la cola
+                     xQueueReceive(buffer, &rcv, pdMS_TO_TICKS(100)); // Espera hasta que haya un elemento en la cola
                     if (variable == rcv)
                     {
                         break;
                     }
                     else
                     {
-                        printf("Item recibido en ciclo: %lu\n", rcv); // Muestra el valor en pantalla
-                        printf("Item recibido en ciclo: %lu\n", variable);
+                        printf("Item recibido en rcv: %lu\n", rcv); // Muestra el valor en pantalla
+                        printf("Item recibido en variable: %lu\n", variable);
                         gpio_set_level(LED_GPIO2, 1);
-                        vTaskDelay(pdMS_TO_TICKS(500)); // Enciende el LED durante 500 ms
+                        vTaskDelay(pdMS_TO_TICKS(250)); // Enciende el LED durante 500 ms
                         gpio_set_level(LED_GPIO2, 0);
-                        vTaskDelay(pdMS_TO_TICKS(500)); // Apaga el LED durante 500 ms
+                        vTaskDelay(pdMS_TO_TICKS(250)); // Apaga el LED durante 500 ms
 
                     }
+                    
                     
                    
                     
@@ -75,6 +71,7 @@ void tarea2(void* z)
 
             }
             else printf("Fuera del if");
+            vTaskDelay(pdMS_TO_TICKS(500)); // Pequeña pausa para evitar rebotes del botón
         }
         
     }
@@ -89,6 +86,6 @@ void app_main()
     gpio_set_direction(LED_GPIO2, GPIO_MODE_OUTPUT);
 
 
-    xTaskCreatePinnedToCore(tarea1, "tarea1", 4096, NULL, 1, NULL, 0); // Crea tarea que escribe valores en la cola
-    xTaskCreatePinnedToCore(tarea2, "tarea2", 4096, NULL, 4, NULL, 0); // Crea tarea que lee valores de la cola.
+    xTaskCreatePinnedToCore(tarea1, "tarea1", 4096, NULL, 4, NULL, 0); // Crea tarea que escribe valores en la cola
+    xTaskCreatePinnedToCore(tarea2, "tarea2", 4096, NULL, 1, NULL, 0); // Crea tarea que lee valores de la cola.
 }
